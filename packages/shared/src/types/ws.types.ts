1c319796656c18b9e1be3653daf6e464fa8410de
fujitashuichi <fujitashuu14@gmail.com>
2026-04-05 10:32:45 +0900

    feature(shared): 通信データの型を作成

---
+++ packages/shared/src/types/ws.types.ts
@@ -0,0 +1,14 @@
+type WsProtocolErrorNames =
+  | "UnsupportedData" | "ProtocolError" | "PolicyViolation" | "InternalServerError"
+
+export type WsResponse =
+  | {
+    ok: false,
+    status: number,
+    errorName: WsProtocolErrorNames
+  }
+  | {
+    ok: true,
+    status: number,
+    data: JSON
+  };
