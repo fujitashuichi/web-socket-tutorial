9c14393f69ad376aaf4e35a1e76a4aa098a7dfe2
fujitashuichi <fujitashuu14@gmail.com>
2026-04-06 15:38:04 +0900

    refactor(ws/onMessage): メッセージイベントの切り出し

--- packages/backend/server/src/websockets/webSocket.ts
+++ packages/backend/server/src/websockets/webSocket.ts
@@ -1,5 +1,9 @@
 import { WebSocketServer } from "ws";
+import type { WebSocket } from "ws";
 import { tryConnect } from "./tryConnect.js";
+import type { WsResponse } from "@app/shared";
+import { randomGreeting } from "../utils/randomGreeting.js";
+import { WsClientEvents } from "./wsClient.js";
 
 export class Socket {
   private socket: WebSocketServer | null;
@@ -10,15 +14,29 @@ export class Socket {
     this.port = port;
   }
 
+
+  wsEvents = (ws: WebSocket) => {
+    return new WsClientEvents(ws);
+  }
+
   connect = async () => {
     const setSocket = (wss: WebSocketServer) => {
       this.socket = wss
     };
-    tryConnect(this.port, 3, setSocket);
+    await tryConnect(this.port, 3, setSocket);
   };
 
 
-  onMessage = () => {
-    this.onMessage();
+  onMessage = async () => {
+    if (!this.socket) return await this.connect();
+
+    const message: WsResponse = {
+      ok: true,
+      status: 1000,
+      data: randomGreeting()
+    }
+    this.socket.on("connection", (ws) => {
+      ws.on("message", this.wsEvents(ws).onmessage(message));
+    })
   }
 }
---
+++ packages/backend/server/src/websockets/wsClient.ts
@@ -0,0 +1,38 @@
+import type { WsResponse } from "@app/shared";
+import type { WebSocket } from "ws";
+import { randomGreeting } from "../utils/randomGreeting.js";
+
+export class WsClientEvents {
+  private readonly ws: WebSocket;
+  constructor(ws: WebSocket) {
+    this.ws = ws;
+  }
+
+  onmessage = (message: WsResponse) => {
+    return (data: any, isBinary: boolean) => {
+      if (isBinary) {
+        const message: WsResponse = {
+          ok: false,
+          status: 1006,
+          errorName: "UnsupportedData"
+        }
+        return this.ws.send(JSON.stringify(message));
+      }
+
+      try {
+        const jsonString = data.toString();
+        const payload = JSON.parse(jsonString);
+        console.log("message:", payload);
+
+        this.ws.send(JSON.stringify(message));
+      } catch (e) {
+        const message: WsResponse = {
+          ok: false,
+          status: 1002,
+          errorName: "UnsupportedData"
+        }
+        this.ws.send(JSON.stringify(message));
+      };
+    }
+  }
+}
